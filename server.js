cd53ab86ae484c2587c2b7f5e45e79637f0cf863
MultiMouse <olmos.luisd@outlook.com>
2023-11-04 20:46:24 -0500

    added index, helpers, and routers

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const api = require('./routes/index.js');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -7,6 +8,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
+app.use('/api', api);
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
