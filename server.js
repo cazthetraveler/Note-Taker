1965c1efa95b97ccfd8487524bb05fa686cd3b21
MultiMouse <olmos.luisd@outlook.com>
2023-11-04 20:30:38 -0500

    added routes for homepage and notes

--- server.js
+++ server.js
@@ -8,3 +8,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+
+app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
