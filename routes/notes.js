6e6705f4a49d4b160843e2a57f08babd4c820df8
MultiMouse <olmos.luisd@outlook.com>
2023-11-04 20:48:14 -0500

    fixed file path mistake on notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -2,6 +2,6 @@ const notes = require('express').Router();
 const {vr: uuidv4} = require('uuid');
 const { readFromFile } = require('../helpers/fsUtils');
 
-notes.get('/', (req, res) => readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data))));
+notes.get('/', (req, res) => readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data))));
 
 module.exports = notes;
