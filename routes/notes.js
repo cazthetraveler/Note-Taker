bde3f5626df4a77f9b00d4379653a67600bbda81
MultiMouse <olmos.luisd@outlook.com>
2023-11-06 22:37:50 -0600

    made a bunch of changes that i forgot lol

--- routes/notes.js
+++ routes/notes.js
@@ -1,7 +1,23 @@
 const notes = require('express').Router();
 const {vr: uuidv4} = require('uuid');
-const { readFromFile } = require('../helpers/fsUtils');
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 
-notes.get('/', (req, res) => readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data))));
+
+notes.get('/notes', (req, res) => readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data))));
+
+notes.post('/api/notes', (req, res) => {
+    const {title, text} = req.body;
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+        };
+    readAndAppend(newNote, '../db/db.json');
+    res.json(`Note added!!`);
+    } else {
+        res.errored(`Dingbat`);
+    };
+});
 
 module.exports = notes;
